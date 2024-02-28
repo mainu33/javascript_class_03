b5fdd076c0a71acf5661d9650ffb681c1789ffc2
mainu33 <130586257+mainu33@users.noreply.github.com>
2024-02-28 09:45:56 +0600

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,78 @@
+
+// document.write("hello developer");
+
+// console.log("hello mainuddin");
+
+//var late confution
+// var name="mainuddin";
+// var name="mainu";
+// document.write(name);
+//console.log(name);
+
+//let
+// let name ="mainu";
+//  name ="mainuddin";
+//  //document.write(name);
+//  console.log(name);
+
+//const keyword
+// const name="mainuddin";
+//  name="mainuddin";
+//  let name="mainu";
+//  console.log(name);
+
+//function 
+// function sum(){
+//       var a=10;
+//       var b=20;
+//       var c=a+b;
+//       document.write(c); 
+    
+// }
+//function sum()
+// function sum(fullname){
+//       document.write("my name is "+fullname + "<br>");
+// }
+// sum("mainuddin");
+// sum("mainu");
+
+//call in function
+//function calls( name, age){
+      //document.write(name + " is " +age+ " years old");
+//}
+
+
+//return
+// function one(frist, last){
+//       var full
+//       full= frist + last;
+//       return full;
+// }
+
+// function two(){
+//       var result;
+//       result = one(10, 10);
+
+//       document.write(result);
+// }
+
+// function test(){
+//       console.log("hello developer");
+// }
+// test()
+
+// function test(){
+//       return "hello developer";
+// }
+// var val=test();
+
+// console.log(val);
+
+// function add(num1, num2){
+//       var sum= num1 + num2;
+//       console.log("the some of the valuse enters "+sum);
+// }
+// add(10, 20);
+
+
+
